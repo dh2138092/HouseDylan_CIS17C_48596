ec8cf474a7ef46c1557de5e24900701db96e65c7
dh2138092 <dh2138092@users.noreply.github.com>
2014-09-05 11:54:43 -0700

    Update prog3a.s

--- Book/prog3a.s
+++ Book/prog3a.s
@@ -1,5 +1,5 @@
 @ prog3a.s - a simple assembler file
-@ .global _start - removed as instructed to demonstrate error
+@ .global _start - removed, as instructed, to demonstrate error
 
 _start:
   MOV R0, #49
