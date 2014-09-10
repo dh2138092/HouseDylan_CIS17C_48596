e5befd0f863ddac51d6c235ab0f64bf799ffed87
dh2138092 <dh2138092@users.noreply.github.com>
2014-09-09 18:09:38 -0700

    Update TriangularMatrix.cpp
    Added another constructor that initializes the matrix's dimensions.

--- Hmwk/TriangularMatrix/TriangularMatrix.cpp
+++ Hmwk/TriangularMatrix/TriangularMatrix.cpp
@@ -12,8 +12,7 @@ int main(int argc, char *argv[])
 
 	// Initialize the dimensions of the 1-d array and triangular matrix
 	int rows = 10;
-	int columns = rows;
-	int columnsPerLine = 10;
+	int columnsPerRow = 10;
 
 	std::cout << "Start of the demo. \n\n\n\n";
 
@@ -22,42 +21,43 @@ int main(int argc, char *argv[])
 	//////////////////////////////////////////////////////////////
 
 	// Initialize a TriangularMatrix class that handles INTEGER data types
-	TriangularMatrix<int> triangularMatrixInt;
+	TriangularMatrix<int> triangularMatrixInt(rows, columnsPerRow);
 
 	// Fill the 1-dimensional array and triangular matrix with random numbers
-	int *array1dInt = triangularMatrixInt.FillArray(columns);
-	int **arrayTriangularInt = triangularMatrixInt.FillArray(rows, array1dInt);
+	int *array1dInt = triangularMatrixInt.FillArray();
+	int **arrayTriangularInt = triangularMatrixInt.FillArray(array1dInt);
 
 	// Print out the arrays
 	std::cout << "INTEGER 1-D array: \n";
-	triangularMatrixInt.PrintArray(array1dInt, columns, columnsPerLine);
+	triangularMatrixInt.PrintArray(array1dInt);
 	std::cout << "INTEGER tri matrix: \n";
-	triangularMatrixInt.PrintArray(arrayTriangularInt, array1dInt, rows);
+	triangularMatrixInt.PrintArray(arrayTriangularInt, array1dInt);
 
 	// Cleanup and get rid of the arrays
-	triangularMatrixInt.DestroyArray(arrayTriangularInt, rows);
+	triangularMatrixInt.DestroyArray(arrayTriangularInt);
 	triangularMatrixInt.DestroyArray(array1dInt);
 
 	std::cout << "\n=============================\n\n\n";
+
 	////////////////////////////////////////////////////////////
 	// Demo the TriangularMatrix class using FLOAT data types //
 	////////////////////////////////////////////////////////////
 
 	// Initialize a TriangularMatrix class that handles FLOAT data types
-	TriangularMatrix<float> triangularMatrixFloat;
+	TriangularMatrix<float> triangularMatrixFloat(rows, columnsPerRow);
 
 	// Fill the 1-dimensional array and triangular matrix with random numbers
-	float *array1dFloat = triangularMatrixFloat.FillArray(columns);
-	float **arrayTriangularFloat = triangularMatrixFloat.FillArray(rows, array1dFloat);
+	float *array1dFloat = triangularMatrixFloat.FillArray();
+	float **arrayTriangularFloat = triangularMatrixFloat.FillArray(array1dFloat);
 
 	// Print out the arrays
 	std::cout << "FLOAT 1-D array: \n";
-	triangularMatrixFloat.PrintArray(array1dFloat, columns, columnsPerLine);
+	triangularMatrixFloat.PrintArray(array1dFloat);
 	std::cout << "FLOAT tri matrix: \n";
-	triangularMatrixFloat.PrintArray(arrayTriangularFloat, array1dFloat, rows);
+	triangularMatrixFloat.PrintArray(arrayTriangularFloat, array1dFloat);
 
 	// Cleanup and get rid of the arrays
-	triangularMatrixFloat.DestroyArray(arrayTriangularFloat, rows);
+	triangularMatrixFloat.DestroyArray(arrayTriangularFloat);
 	triangularMatrixFloat.DestroyArray(array1dFloat);
 
 	std::cout << "\n\nEnd of the demo. \n\n";
