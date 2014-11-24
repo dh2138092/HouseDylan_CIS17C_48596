84c8b155b63fc15e2c0578fbef950ac0d6757a9d
Dylan House <housed@users.noreply.github.com>
2014-11-24 10:57:58 -0800

    Added some test code

--- Hmwk/Binary Search Tree/main.cpp
+++ Hmwk/Binary Search Tree/main.cpp
@@ -1,8 +1,47 @@
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+
 #include "BinarySearchTree.h"
 
 int main(int argc, char* argv[])
 {
 	BST<int> *tree = new BST<int>();
+
+	srand((unsigned int)time(0));
+	int num = 0;
+	int const SIZE = 10;
+
+	for (int i = 1; i <= SIZE; i++)
+	{
+		num = rand() % 15;
+		tree->insert(num);
+	}
+
+	tree->preorder();
+	std::cout << "\n\n";
+	tree->postorder();
+	std::cout << "\n\n";
+	tree->inorder();
+	std::cout << "\n\n";
+
+	for (int i = 1; i <= SIZE; i++)
+	{
+		num = rand() % 15;
+		std::cout << "Attempting to remove " << num << " from tree\n";
+		tree->findAndDeleteByMerging(num);
+		std::cout << '\n';
+	}
+	
+	std::cout << "\n\n";
+	tree->preorder();
+	std::cout << "\n\n";
+	tree->postorder();
+	std::cout << "\n\n";
+	tree->inorder();
+	std::cout << "\n\n";
+	
 	delete tree;
+
 	return 0;
 }
